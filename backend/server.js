d69b00cefae7b1ee8db38ecf6226c3cb6d4245d5
Natasha Sirois <sirois.natasha@gmail.com>
2019-02-11 15:53:08 -0500

    backend login and signup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 let express = require("express");
 let cors = require("cors");
+let sha256 = require("sha256");
 let bodyParser = require("body-parser");
+
+const url = "mongodb://admin:admin123@ds111993.mlab.com:11993/alibay";
+let dbs = undefined;
 
 let app = express();
 app.use(cors());
 app.use(bodyParser.raw({ type: "*/*" }));
+
 app.use((req, res, next) => {
+  MongoClient.connect(url, (err, allDbs) => {
+    dbs = allDbs;
+  });
   try {
     req.body = JSON.parse(req.body.toString());
     next();
@@ -15,11 +23,58 @@ app.use((req, res, next) => {
 });
 
 app.post("/signup", (res, req) => {
-  res.send({ success: true });
+  let db = dbs.db("alibay");
+  let findUser = db
+    .collection("user")
+    .find({ username: req.body.username })
+    .toArray((err, result) => {
+      console.log(result, err);
+      return result;
+    });
+  if (findUser.lenght < 1) {
+    res.send(JSON.stringify({ success: false }));
+  } else {
+    let user = {
+      username: req.body.username,
+      password: sha256(req.body.password)
+    };
+    db.collection("users").insertOne(user, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+    });
+    res.send(JSON.stringify({ success: true }));
+  }
 });
 
 app.post("/login", (res, req) => {
-  res.send({ success: true });
+  let db = dbs.db("alibay");
+  let userArr = db
+    .collection("users")
+    .find({ username: req.body.username })
+    .toArray((err, result) => {
+      return result;
+    });
+  if (userArr.lenght > 0) {
+    let user = userArr[0];
+    if (user.password === sha256(req.body.password)) {
+      res.send(
+        JSON.stringify({
+          success: true,
+          user: {
+            userId: user._id,
+            username: user.username,
+            location: user.location
+          }
+        })
+      );
+    } else {
+      res.send(JSON.stringify({ success: false }));
+    }
+  } else {
+    res.send(JSON.stringify({ success: false }));
+  }
 });
 
 app.get("/allcupcakes", (req, res) => {
