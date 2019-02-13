3c569a4e8ccaeb12026aa3b4c21de8c9c6ca9280
thewheelturner <vincent.savard90@gmail.com>
2019-02-13 15:47:39 -0500

    stylish

--- frontend/src/Components/single-cupcake/index.js
+++ frontend/src/Components/single-cupcake/index.js
@@ -3,24 +3,79 @@ import { connect } from "react-redux";
 import "../../App.js";
 import "./single-cupcake.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+`;
+
+const StyledContainer = styled.div`
+  display: flex;
+  border-style: dotted;
+  border-radius: 5%;
+  padding: 5px;
+  width: 400;
+  margin-bottom: 2%;
+`;
+
+const StyledImageContainer = styled.img`
+  width: 250px;
+  height: 250px;
+  border-radius: 5%;
+`;
+
+const StyledInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 325px;
+  font-size: 20px;
+  text-align: left;
+`;
+
+const StyledName = styled.div`
+  font-size: 35px;
+  text-decoration: none;
+`;
+
+const StyledCategory = styled.div`
+  text-decoration: none;
+  font-size: 20px;
+`;
+
+const StyledPrice = styled.div`
+  font-size: 20px;
+`;
+
+const StyledStock = styled.div`
+  font-size: 20px;
+`;
 
 class SingleCupcake extends Component {
   render() {
     return (
-      <div className="cupcake">
-        <img
-          src={`http://localhost:4000/${this.props.cupcake.picture}`}
-          alt="one-cupcake"
-        />
-        <p className="name">
-          <Link to={"/getcupcake/" + this.props.cupcake._id}>
-            {this.props.cupcake.name}
-          </Link>
-        </p>
-        <p className="category">{this.props.cupcake.category}</p>
-        <p className="price">{this.props.cupcake.price}$/each</p>
-        <p className="seller">{this.props.cupcake.userID}</p>
-      </div>
+      <MainContainer>
+        <StyledContainer>
+          <StyledImageContainer
+            src={`http://localhost:4000/${this.props.cupcake.picture}`}
+            alt="one-cupcake"
+          />
+          <StyledInfoContainer>
+            <StyledName className="name">
+              <Link to={"/getcupcake/" + this.props.cupcake._id}>
+                {this.props.cupcake.name}
+              </Link>
+            </StyledName>
+            <StyledCategory>
+              {"Category: "}
+              {this.props.cupcake.category}
+            </StyledCategory>
+            <StyledPrice>
+              {"Price: "}
+              {this.props.cupcake.price}$/each
+            </StyledPrice>
+          </StyledInfoContainer>
+        </StyledContainer>
+      </MainContainer>
     );
   }
 }
