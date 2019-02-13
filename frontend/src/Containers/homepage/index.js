282e4ae6a90b91b0caa26c667b43610e7dba172e
Yann <yboisvenue@gmail.com>
2019-02-12 22:03:16 -0500

    commented out the Search. will fix 02/13

--- frontend/src/Containers/homepage/index.js
+++ frontend/src/Containers/homepage/index.js
@@ -1,23 +1,16 @@
 import React, { Component } from "react";
 import "./homepage.css";
 import Login from "../../Components/login";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 import firebase from "../../loginGoogle.js";
-=======
 // import firebase from "firebase";
 // var provider = new firebase.auth.GoogleAuthProvider();
 // firebase.auth().signInWithPopup(provider);
->>>>>>> Stashed changes
-=======
+
 // import firebase from "firebase";
 // var provider = new firebase.auth.GoogleAuthProvider();
 // firebase.auth().signInWithPopup(provider);
->>>>>>> Stashed changes
-=======
->>>>>>> efbca67bd7e292191d7063da6911ddc776df7c89
 
 class Homepage extends Component {
   render() {
