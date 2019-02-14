5fdc93c6f68c1abba9df538e9614e2d7a9f278fa
Natasha Sirois <sirois.natasha@gmail.com>
2019-02-14 11:30:09 -0500

    changed form style

--- frontend/src/Components/add-cupcake/index.js
+++ frontend/src/Components/add-cupcake/index.js
@@ -1,11 +1,43 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const StyledContainer = styled.div`
+  border-radius: 2%;
+  margin-top: 100px;
+  margin-bottom: 50px;
+  border-style: solid;
+  border-color: #df3b57;
+  border-width: 1px;
+  background: #fcedef;
+  padding: 30px;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 25px;
+  color: #df3b57;
+`;
+
+const StyledInput = styled.input`
+  border-radius: 5%;
+  border-style: none;
+  padding: 5px;
+  font-size: 15px;
+`;
+
+const StyledButton = styled.input`
+  margin-top: 15px;
+  width: 150px;
+  height: 51px !important;
+  font-size: 15px;
+  background-color: #ffcacb;
+  border-radius: 5px;
+  border: 1px solid #000000;
+`;
+
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   margin-top: 3%;
-  margin-top: 100px;
 `;
 
 const StyledLabel = styled.label`
@@ -32,11 +64,11 @@ const StyledFormItem = styled.div`
   }
 `;
 const StyledTextarea = styled.textarea`
-  height: 128px;
+  border-radius: 2%;
+  border-style: none;
+  height: 90px;
   width: 400px;
-`;
-const StyledItemUser = styled.div`
-  margin-left: 25%;
+  font-size: 15px;
 `;
 
 class AddCupcake extends Component {
@@ -106,6 +138,7 @@ class AddCupcake extends Component {
   render() {
     console.log(this.state);
     return (
+<<<<<<< Updated upstream
       <div>
         <div id="addacupcake_form_div">
           <StyledForm>
@@ -199,6 +232,100 @@ class AddCupcake extends Component {
           </StyledForm>
         </div>
       </div>
+=======
+      <StyledContainer>
+        <StyledH1>Sell your special cupcake. Fill this form!</StyledH1>
+        <StyledForm>
+          <StyledFormItem>
+            <StyledLabel for="name">
+              Wonderful name <span style={{ fontSize: 10 }}>(max 100 KB)</span>:
+            </StyledLabel>
+            <StyledInput
+              type="text"
+              name="name"
+              placeholder="Product's name"
+              value={this.state.name}
+              onChange={event => this.handleChange(event)}
+              style={{ width: 284 }}
+            />
+          </StyledFormItem>
+          <StyledFormItem>
+            <StyledLabel for="picture">
+              Picture of your magnificient cupcake:
+            </StyledLabel>
+            <input
+              type="file"
+              name="picture"
+              onChange={this.handleFileInputChange}
+            />
+          </StyledFormItem>
+          <StyledFormItem>
+            <StyledLabel for="price">Price:</StyledLabel>
+            <StyledInput
+              type="number"
+              name="price"
+              min="0"
+              max="999"
+              placeholder="0"
+              onChange={event => this.handlePriceChange(event)}
+              style={{ width: 50 }}
+            />
+          </StyledFormItem>
+          <StyledFormItem>
+            <StyledLabel for="category">Your creation's category:</StyledLabel>
+            <select
+              class="select_display"
+              name="category"
+              onChange={event => this.handleChange(event)}
+            >
+              <option value="" disabled selected>
+                Please select a flavour!
+              </option>
+              <option value="Vanilla">Vanilla</option>
+              <option value="Chocolate">Chocolate</option>
+              <option value="Carrot">Carrot</option>
+              <option value="Coconut">Coconut</option>
+              <option value="Cookies and cream">Cookies and cream</option>
+              <option value="Vegan">Vegan</option>
+              <option value="Other frivolity">Other frivolity</option>
+            </select>
+          </StyledFormItem>
+          <StyledFormItem>
+            <StyledLabel for="price">
+              A description of your delicious treat:
+            </StyledLabel>
+            <StyledTextarea
+              name="description"
+              placeholder="Fantastic description"
+              onChange={event => this.handleChange(event)}
+            />
+          </StyledFormItem>
+          <StyledFormItem>
+            <StyledLabel for="price">
+              Number of fabulous cupackes available:
+            </StyledLabel>
+            <StyledInput
+              class="number_input"
+              type="number"
+              name="stock"
+              min="0"
+              max="999"
+              placeholder="0"
+              onChange={event => this.handleChange(event)}
+              style={{ width: 50 }}
+            />
+          </StyledFormItem>
+          <StyledFormItem>
+            <StyledButton
+              class="submit-button"
+              type="submit"
+              value="Add your cupcake"
+              onClick={this.handleSubmit}
+            />
+          </StyledFormItem>
+        </StyledForm>
+      </StyledContainer>
+>>>>>>> Stashed changes
     );
   }
 }
